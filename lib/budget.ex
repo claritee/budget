421f8262603e1b2ad0e0ee3512d190c2c51217f7
Claire Tran <c.tran83@gmail.com>
2018-01-24 13:11:19 +1100

    updating with csv parsing, filtering and normalising

--- lib/budget.ex
+++ lib/budget.ex
@@ -5,13 +5,28 @@ defmodule Budget do
     File.read!("lib/transactions.csv")
     |> parse
     |> filter
+    |> normalize
   end
 
   defp parse(string) do
-  	CSV.parse_string(string)
+    string
+    |> String.replace("\r", "")
+    CSV.parse_string(string)
   end
 
   defp filter(rows) do
-  	Enum.map(rows, &Enum.drop(&1, 1))
+    Enum.map(rows, &Enum.drop(&1, 1))
+  end
+
+  defp normalize(rows) do
+    Enum.map(rows, &parse_amount(&1))
+  end
+
+  defp parse_amount([date, description, amount]) do
+    [date, description, parse_to_flat(amount)]
+  end
+
+  defp parse_to_flat(string) do
+    String.to_float(string)
   end
 end
